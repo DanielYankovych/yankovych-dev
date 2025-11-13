41260696126deeab9cf45af70e7a3d1863ca97f9
Daniel <d.yankovych@gmail.com>
2025-11-13 14:42:56 -0800

    wip

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-3 w-full min-h-screen items-center justify-center font-sans bg-black">
+      <span className="font-medium text-3xl">Daniyel Yankovych's Site</span>
+      <span className="text-2xl animate-pulse">Coming soon...</span>
+    </div>
+  );
+}
