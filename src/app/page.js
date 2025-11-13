6e35eb02a49e9abbe095d6ffc9eabe9b40fa468f
Daniel <d.yankovych@gmail.com>
2025-11-13 15:59:08 -0800

    wip (UI)

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col gap-3 w-full min-h-screen items-center justify-center font-inter bg-black">
+    <div className="flex flex-col gap-3 w-full h-dvh items-center justify-center font-inter bg-black">
       <span className="font-medium text-3xl">Daniyel Yankovych's Site</span>
       <span className="text-2xl animate-pulse">Coming soon...</span>
     </div>
