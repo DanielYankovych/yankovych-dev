314f7dc22e14e40ed8ce561d91bacc5f7fe34e9b
Daniel <d.yankovych@gmail.com>
2025-11-26 23:17:00 -0800

    swiper for tech stack section + minor UI update

--- src/components/techStack/techStack.jsx
+++ src/components/techStack/techStack.jsx
@@ -3,8 +3,7 @@
 import React, { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import { PROGRAMMING_LANGUAGES, TOOLS } from "@/data";
-import { TechStackBox } from "@/components/techStack/techStackBox";
+import { FRAMEWORKS, PROGRAMMING_LANGUAGES, TOOLS } from "@/data";
 import { TechStackSwiper } from "@/components/techStack/techStackSwiper";
 
 export const TechStack = () => {
@@ -27,7 +26,7 @@ export const TechStack = () => {
 
       gsap.fromTo(
         paragraphRef.current,
-        { y: 80 },
+        { y: 50 },
         {
           y: 0,
           duration: 2,
@@ -40,7 +39,7 @@ export const TechStack = () => {
       );
 
       gsap.to(paragraphRef.current, {
-        yPercent: -80,
+        yPercent: -50,
         ease: "none",
         scrollTrigger: {
           trigger: sectionRef.current,
@@ -52,7 +51,7 @@ export const TechStack = () => {
 
       gsap.fromTo(
         techStackRef.current,
-        { y: 60 },
+        { y: 50 },
         {
           y: 0,
           duration: 2,
@@ -65,7 +64,7 @@ export const TechStack = () => {
       );
 
       gsap.to(techStackRef.current, {
-        yPercent: -60,
+        yPercent: -50,
         ease: "none",
         scrollTrigger: {
           trigger: techStackRef.current,
@@ -77,7 +76,7 @@ export const TechStack = () => {
 
       gsap.fromTo(
         frameworksRef.current,
-        { y: 150 },
+        { y: 120 },
         {
           y: 0,
           ease: "power1.inOut",
@@ -94,7 +93,7 @@ export const TechStack = () => {
 
       gsap.fromTo(
         languagesRef.current,
-        { y: 150 },
+        { y: 120 },
         {
           y: 0,
           ease: "power1.inOut",
@@ -111,7 +110,7 @@ export const TechStack = () => {
 
       gsap.fromTo(
         toolsRef.current,
-        { y: 150 },
+        { y: 120 },
         {
           y: 0,
           ease: "power1.inOut",
@@ -145,7 +144,7 @@ export const TechStack = () => {
 
       <div
         ref={techStackRef}
-        className="text-dark text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl md:-ml-1 leading-none pb-16 md:py-16 text-center"
+        className="text-dark text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl md:-ml-1 leading-none pb-16 pt-0 md:py-16 text-center"
       >
         My tech stack
       </div>
@@ -156,19 +155,11 @@ export const TechStack = () => {
         </div>
       </div>
 
-      <div ref={frameworksListRef} className="w-full overflow-hidden">
-        {/*<div className="w-auto flex justify-start md:justify-center items-start gap-6 md:gap-20 overflow-x-auto no-scrollbar py-8 md:py-12">*/}
-        {/*  {FRAMEWORKS.map((framework, idx) => (*/}
-        {/*    <div key={idx} ref={(el) => (frameworksRef.current[idx] = el)}>*/}
-        {/*      <TechStackBox*/}
-        {/*        name={framework.name}*/}
-        {/*        logo={framework.logo}*/}
-        {/*        classNames={idx === 2 && "!p-3"}*/}
-        {/*      />*/}
-        {/*    </div>*/}
-        {/*  ))}*/}
-        {/*</div>*/}
-        <TechStackSwiper ref={frameworksRef} />
+      <div
+        ref={frameworksListRef}
+        className="w-full overflow-hidden py-8 md:py-12"
+      >
+        <TechStackSwiper data={FRAMEWORKS} ref={frameworksRef} />
       </div>
 
       <div className="flex justify-start items-center text-lg md:text-xl font-fixel-medium pt-8 md:pt-16">
@@ -176,14 +167,11 @@ export const TechStack = () => {
           Programming languages
         </div>
       </div>
-      <div ref={languagesListRef} className="w-full overflow-hidden">
-        <div className="w-auto flex justify-start md:justify-center items-start gap-6 md:gap-20 overflow-x-auto overflow-y-hidden no-scrollbar py-8 md:py-12">
-          {PROGRAMMING_LANGUAGES.map((language, idx) => (
-            <div key={idx} ref={(el) => (languagesRef.current[idx] = el)}>
-              <TechStackBox name={language.name} logo={language.logo} />
-            </div>
-          ))}
-        </div>
+      <div
+        ref={languagesListRef}
+        className="w-full overflow-hidden py-8 md:py-12"
+      >
+        <TechStackSwiper data={PROGRAMMING_LANGUAGES} ref={languagesRef} />
       </div>
 
       <div className="flex justify-start items-center text-lg md:text-xl font-fixel-medium pt-8 md:pt-16">
@@ -191,14 +179,14 @@ export const TechStack = () => {
           Tools
         </div>
       </div>
-      <div ref={toolsListRef} className="w-full overflow-hidden">
-        <div className="w-auto flex justify-start items-start gap-6 md:gap-20 overflow-x-auto overflow-y-hidden no-scrollbar py-8 md:py-12">
-          {TOOLS.map((tool, idx) => (
-            <div key={idx} ref={(el) => (toolsRef.current[idx] = el)}>
-              <TechStackBox name={tool.name} logo={tool.logo} />
-            </div>
-          ))}
-        </div>
+      <div ref={toolsListRef} className="w-full overflow-hidden py-8 md:py-12">
+        <TechStackSwiper
+          data={TOOLS}
+          slidesPerViewDesktop={7}
+          initialSlide={3}
+          autoplay
+          ref={toolsRef}
+        />
       </div>
     </div>
   );
